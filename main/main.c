ee5027619b0b9bbe367eb361785e4bcb4f3911de
Llarence <1010kai.t1010@gmail.com>
2025-10-18 19:20:26 -0700

    simple buttons

--- main/main.c
+++ main/main.c
@@ -1,16 +1,16 @@
+#include "bsp_board.h"
+#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_log.h"
-#include "bsp_board.h"
 #include "lv_port.h"
 
+#include "view/view.h"
 
-void app_main(void)
-{
-    ESP_ERROR_CHECK(bsp_board_init());
-    lv_port_init();
+void app_main(void) {
+  ESP_ERROR_CHECK(bsp_board_init());
+  lv_port_init();
 
-    lv_port_sem_take();
-    ui_init();
-    lv_port_sem_give();
+  lv_port_sem_take();
+  view_init();
+  lv_port_sem_give();
 }
---
+++ main/view/view.h
@@ -0,0 +1,8 @@
+#ifndef VIEW_H
+#define VIEW_H
+
+#include "lvgl.h"
+
+void view_init(void);
+
+#endif
